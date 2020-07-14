56ac932b35a65316db4458e43385733497e8577d
Nathan Hejnicki <hejman08@gmail.com>
2020-07-14 13:46:07 -0400

    Fix bug with UDT generating as NCM Training

--- result/training_bits/UDTTraining.php
+++ result/training_bits/UDTTraining.php
@@ -3,19 +3,12 @@ global $UDTTrainDays;
 if ($UDTTrainDays > 0)
     {
         $html .= "<h2>" . dayHeader($UDTTrainDays, "UDT User Training") . "</h2>\n";
-        $html .= "<h3>NCM Training Topics</h3>\n";
-        $html .= "<ul><li>NCM user permissions</li>\n";
-        $html .= "<li>Adding nodes to NCM</li>\n";
-        $html .= "<ul><li>NCM connection profiles</li>\n";
-        $html .= "<li>NCM device templates</li></ul>\n";
-        $html .= "<li>NCM jobs and scheduling backups</li>\n";
-        $html .= "<li>Network Insight for Cisco ASA</li>\n";
-        $html .= "<li>Executing scripts and bulk changes</li>\n";
-        $html .= "<li>Real-time configuration change detection</li>\n";
-        $html .= "<li>Compliance policy reports</li>\n";
-        $html .= "<li>End of sales/end of support reporting</li>\n";
-        $html .= "<li>Cisco & Juniper firmware vulnerabilities reporting</li>\n";
-        $html .= "<li>Introduction to configuration change templates</li>\n";
+        $html .= "<h3>UDT Training Topics</h3>";
+        $html .= "<ul><li>Adding nodes and ports to UDT</li>\n";
+        $html .= "<li>Mapping ports to hostname, MAC address and/or IP address</li>\n";
+        $html .= "<li>Monitoring Active Directory domain controllers for user login data</li>\n";
+        $html .= "<li>White list management</li>\n";
+        $html .= "<li>Watch list management</li>\n";
         $html .= "</ul>";
     }
 ?>
