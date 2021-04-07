1bdb4711ac4d169b93f4cf8c9cd38f657f6c4e6a
Nathan Hejnicki <hejman08@gmail.com>
2021-04-07 11:45:35 -0400

    bug squash

--- result/config_bits/runbook.php
+++ result/config_bits/runbook.php
@@ -1,4 +1,7 @@
-<?php if ($RunbookDays > 0) {
+<?php 
+
+global $RunbookDays;
+if ($RunbookDays > 0) {
         $html .= "<h2>" . dayHeader($RunbookDays, "Runbook Documentation Preparation") . "</h2>\n"
         . "<p>The Runbook documentation produced by Loop1, LLC will cover key administrative functions for the products.  "
         . " This documentation will serve as a reference to managing what has been configured in the client’s SolarWinds "
