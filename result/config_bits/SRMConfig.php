a9ddb27295eb27e7ab80f6f3cecf04c940c350c6
Nathan Hejnicki <hejman08@gmail.com>
2021-06-03 09:38:13 -0400

    Fix extra SRM header showing up

--- result/config_bits/SRMConfig.php
+++ result/config_bits/SRMConfig.php
@@ -3,30 +3,15 @@ global $SRMDays;
 if ($SRMDays > 0)
     {
         $html .= "<h2>" . dayHeader($SRMDays, "SRM Best Practices Configuration") . "</h2>\n"
-        . "<h3>SRM Configuration Tasks</h3>\n";
-        if ($chkSRMProfiler == true) {
-            $html .= "<ul><li>Configuration of SRM software (as needed)</li>\n"
-            . "<ul><li>SRM by Profiler core application</li>\n"
-            . "<li>SRM Additional Polling Engine(s) (if needed)</li>\n"
-            . "<li>SRM Orion module</li></ul>\n"
-            . "<li>Configuration of credentials</li>\n"
-            . "<li>Validation/Import of storage arrays</li>\n"
-            . "<li>Validation/Import of fiber channel switches (Profiler only)</li>\n"
-            . "<li>Configuration of groups</li>\n"
-            . "<li>Configuration of alerts</li>\n"
-            . "<li>Configuration of reports/schedules</li>\n"
-            . "<li>AppStack Overview</li>\n"
-            . "<li>Orion NPM/SAM integration</li>\n"
-            . "<ul><li>Orion SRM module</li>\n"
-            . "<ul><li>Addition of storage devices</li>\n"
-            . "<li>SRM Profiler integration (if Profiler is in use)</li></ul></ul>\n"
-            . "</ul>";
-        } else {
-            $html .= "<h3>SRM Standard Configuration Tasks</h3>\n"
+            . "<h3>SRM Standard Configuration Tasks</h3>\n"
             . "<ul>\n"
             . "<li>Configuration of credentials</li>\n"
-            . "<li>Validation/Import of storage arrays</li>\n"
-            . "<li>Advanced alert configuration</li>\n"
+            . "<li>Validation/Import of storage arrays</li>\n";
+            if ($chkSRMProfiler == true) { 
+                $html .= "<li>Validation/Import of fiber channel switches (Profiler only)</li>\n"
+                . "<li>SRM Profiler integration with Orion Platform</li></ul></ul>\n"; 
+            }
+            $html .= "<li>Advanced alert configuration</li>\n"
             . "<ul>\n"
             . "<li>Noise Reduction</li>\n"
             . "<li>Notification</li>\n"
