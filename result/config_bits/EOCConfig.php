32653ce521e06913d3b591bb8daf9a37294c763f
Nathan Hejnicki <hejman08@gmail.com>
2021-07-08 10:45:53 -0400

    Fix EOC config displaying PMan bits

--- result/config_bits/EOCConfig.php
+++ result/config_bits/EOCConfig.php
@@ -3,28 +3,14 @@ global $EOCDays;
 if ($EOCDays > 0)
     {
         $html .= "<h2>" . dayHeader($EOCDays, "EOC Best Practices Configuration") . "</h2>\n"
-        . "<h3>PM Standard Configuration Tasks</h3>\n"
-        . "<ul>\n"
-        . "<li>Import WSUS/SCCM servers</li>\n"
-        . "<li>Configure Automation servers (if present)</li>\n"
-        . "<li>Configure credentials</li>\n"
-        . "<li>Deploy/Configure WMI providers</li>\n"
-        . "<li>Configure computer groups</li>\n"
-        . "<li>Scan configuration</li>\n"
-        . "<li>Configure MS updates</li>\n"
-        . "<li>Configure 3rd Party updates</li>\n"
-        . "<ul>\n"
-        . "<li>Custom packages if required and as time allows</li>\n"
-        . "<li>Scheduling</li>\n"
-        . "<li>Validation</li>\n"
-        . "<li>Reporting</li>\n"
-        . "</ul>\n"
-        . "<li>Testing &amp; Validation</li>\n"
-        . "</ul>\n"
-        . "<h3>PM Optional Configuration Tasks</h3>\n"
-        . "<ul>\n"
-        . "<li>Configuration of compliance reporting</li>\n"
-        . "</ul>\n";
+        . "<h3>EOC Configuration Tasks\n</h3>"
+        . "<ul><li>Configuration of EOC software</li>\n"
+        . "<li>User configuration</li>\n"
+        . "<li>Integration with SolarWinds environments</li>\n"
+        . "<li>Custom property configuration/validation</li>\n"
+        . "<li>Custom view configuration</li>\n"
+        . "<li>Custom map configuration</li></ul>\n";
+        
         
     }
 ?>
